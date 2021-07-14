34929b0ad88cd7b619f8a50c42524def67ea0945
Kyle Schrader <kyle.schrader97@gmail.com>
2021-07-14 12:58:43 -0500

    finished while and do/while loops exercise

--- js/while.js
+++ js/while.js
@@ -1,9 +1,28 @@
 "use strict";
 
 
-// var number = 1;
-// while (number < 65000) {
-//     number *= 2;
-//     console.log(number);
-// }
+var number = 1;
+while (number < 65000) {
+    number *= 2;
+    console.log(number);
+}
+
+
+var allCones = Math.floor(Math.random() * 50) + 50;
+
+console.log("We have " + allCones + " for sale.");
+
+do {
+    var purchaseAttempt = Math.floor(Math.random() * 5) + 1;
+
+    if (purchaseAttempt <= allCones) {
+        allCones = (allCones - purchaseAttempt);
+    } else {
+        console.log("Shoot! We don't have that many cones. We have " + allCones + " cones left.");
+    }
+
+    console.log("You bought " + purchaseAttempt + " cones. There are " + allCones + " left!");
+} while (allCones > 0);
+
+console.log("Yay! We sold them all!");
 
