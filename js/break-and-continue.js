7ffe5d2e556a48dec462b592c9e203b77d73f424
Kyle Schrader <kyle.schrader97@gmail.com>
2021-07-15 16:29:51 -0500

    making progress on break-and-continue.js. saving progress.

---
+++ js/break-and-continue.js
@@ -0,0 +1,11 @@
+
+
+
+var userOddNumber = prompt("Please enter an ODD number between 1 and 50:");
+
+if (userOddNumber % 2 === 0) {
+    alert("That is an even number. Please refresh the page and enter an ODD number.")
+}
+
+console.log("Number to skip is: " + userOddNumber);
+
