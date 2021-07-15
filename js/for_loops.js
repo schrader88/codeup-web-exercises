e23c7ef7c7aa6d8ab0c96043f1ee88c9a72134c3
Kyle Schrader <kyle.schrader97@gmail.com>
2021-07-15 12:03:27 -0500

    finished with exercise 1 on for loops. moving on to exercise 2.

--- js/for_loops.js
+++ js/for_loops.js
@@ -1,3 +1,14 @@
-"use strict";
 
-alert("this works");
+
+// function showMultiplicationTable(number) {
+//     for (i = 1; i <= 10; i++){
+//         console.log(number + " x " + i + " = " + (number * i));
+//     }
+// }
+//
+// showMultiplicationTable(7);
+
+//var numberBetween = Math.floor(Math.random() * 180) + 20;
+
+
+
