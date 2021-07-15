cd57bd69433477ad451c6046d8e9a8f5ca7979b8
Kyle Schrader <kyle.schrader97@gmail.com>
2021-07-15 12:48:57 -0500

    finished 2nd exercise for for_loops.js. moving to 3rd.

--- js/for_loops.js
+++ js/for_loops.js
@@ -8,7 +8,18 @@
 //
 // showMultiplicationTable(7);
 
-//var numberBetween = Math.floor(Math.random() * 180) + 20;
-
-
+// function randomNumberGenerator() {
+//     for (i = 1; i <= 10; i++){
+//
+//         var numberBetween = Math.floor(Math.random() * 180) + 20;
+//
+//         if (numberBetween % 2 === 0) {
+//             console.log(numberBetween + " is even.");
+//         } else {
+//             console.log(numberBetween + " is odd.");
+//         }
+//     }
+// }
+//
+// randomNumberGenerator();
 
