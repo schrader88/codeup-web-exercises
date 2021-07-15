6fbe07d7f8c34407f872ea8ab16807ddd6d56f68
Kyle Schrader <kyle.schrader97@gmail.com>
2021-07-15 11:12:45 -0500

    commenting out content.

--- js/while.js
+++ js/while.js
@@ -1,28 +1,28 @@
 "use strict";
 
 
-var number = 1;
-while (number < 65000) {
-    number *= 2;
-    console.log(number);
-}
-
-
-var allCones = Math.floor(Math.random() * 50) + 50;
-
-console.log("We have " + allCones + " for sale.");
-
-do {
-    var purchaseAttempt = Math.floor(Math.random() * 5) + 1;
-
-    if (purchaseAttempt <= allCones) {
-        allCones = (allCones - purchaseAttempt);
-        console.log("You bought " + purchaseAttempt + " cone(s). There are " + allCones + " cone(s) left!");
-    } else {
-        console.log("Shoot! We don't have " + purchaseAttempt + " cone(s). We have " + allCones + " cone(s) left.");
-    }
-
-} while (allCones > 0);
-
-console.log("Yay! We sold them all!");
+// var number = 1;
+// while (number < 65000) {
+//     number *= 2;
+//     console.log(number);
+// }
+//
+//
+// var allCones = Math.floor(Math.random() * 50) + 50;
+//
+// console.log("We have " + allCones + " for sale.");
+//
+// do {
+//     var purchaseAttempt = Math.floor(Math.random() * 5) + 1;
+//
+//     if (purchaseAttempt <= allCones) {
+//         allCones = (allCones - purchaseAttempt);
+//         console.log("You bought " + purchaseAttempt + " cone(s). There are " + allCones + " cone(s) left!");
+//     } else {
+//         console.log("Shoot! We don't have " + purchaseAttempt + " cone(s). We have " + allCones + " cone(s) left.");
+//     }
+//
+// } while (allCones > 0);
+//
+// console.log("Yay! We sold them all!");
 
