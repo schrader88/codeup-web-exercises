841bff2ff5f35a71312a9032ee43e2693c553bba
Kyle Schrader <kyle.schrader97@gmail.com>
2021-07-18 16:29:08 -0500

    finished break and continue. refactoring before push.

--- js/break-and-continue.js
+++ js/break-and-continue.js
@@ -1,23 +1,25 @@
+while (true) {
+    var userOddNumber = prompt("Please enter an ODD number between 1 and 50:");
 
+    console.log("Number to skip is: " + userOddNumber)
 
-
-var userOddNumber = prompt("Please enter an ODD number between 1 and 50:");
-
-console.log("Number to skip is: " + userOddNumber);
+    if (userOddNumber % 2 === 1) {
+        break;
+    } else if (userOddNumber % 2 === 0) {
+        alert("That is an even number. Please enter an ODD number.");
+        continue;
+    }
+}
 
 for (i = 1; i <= 49; i = i + 2) {
 
-    if (userOddNumber % 2 === 0) {
-        alert("That is an even number. Please enter an ODD number.");
-        break;
-    } else if (userOddNumber == i) {
+    if (userOddNumber == i) {
         console.log("Yikes! Skipping number: " + i);
         continue;
     }
 
     console.log("Here is an odd number: " + i);
 }
-
 
 
 
