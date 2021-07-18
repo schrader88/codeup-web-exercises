c5689a3962b057b236c61ab71127ad6ab637f05e
Kyle Schrader <kyle.schrader97@gmail.com>
2021-07-18 16:14:44 -0500

    almost finished with break and continue exercise. working on breaking back to prompt.

--- js/break-and-continue.js
+++ js/break-and-continue.js
@@ -3,9 +3,23 @@
 
 var userOddNumber = prompt("Please enter an ODD number between 1 and 50:");
 
-if (userOddNumber % 2 === 0) {
-    alert("That is an even number. Please refresh the page and enter an ODD number.")
+console.log("Number to skip is: " + userOddNumber);
+
+for (i = 1; i <= 49; i = i + 2) {
+
+    if (userOddNumber % 2 === 0) {
+        alert("That is an even number. Please enter an ODD number.");
+        break;
+    } else if (userOddNumber == i) {
+        console.log("Yikes! Skipping number: " + i);
+        continue;
+    }
+
+    console.log("Here is an odd number: " + i);
 }
 
-console.log("Number to skip is: " + userOddNumber);
+
+
+
+
 
