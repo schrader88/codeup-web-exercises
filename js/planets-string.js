6fcce8741c2f17645549ee5f5707067438076860
Kyle Schrader <kyle.schrader97@gmail.com>
2021-07-19 11:49:40 -0500

    finished with planets string exercise. finished with entire manipulation exercise. pushing and working on bonus material.

--- js/planets-string.js
+++ js/planets-string.js
@@ -2,7 +2,6 @@
     "use strict";
 
     var planetsString = "Mercury|Venus|Earth|Mars|Jupiter|Saturn|Uranus|Neptune";
-    var planetsArray;
 
     /**
      * TODO:
@@ -10,8 +9,9 @@
      * planetsArray.
      * console.log planetsArray to check your work
      */
+    var planetsArray = planetsString.split("|");
 
-    // console.log(planetsArray);
+    console.log(planetsArray);
 
     /**
      * TODO:
@@ -23,5 +23,9 @@
      * list. You will need an opening AND closing <ul> tags around the entire
      * string, and <li> tags around each planet.
      */
+
+    var secondPlanetsString = planetsArray.join("<br>");
+
+    console.log(secondPlanetsString);
 
 })();
