fcf7955f7b47af56dee669cc2192b6cd30ec4042
Kyle Schrader <kyle.schrader97@gmail.com>
2021-07-20 14:28:14 -0500

    beginning objects-bonus. first commit.

---
+++ js/objects-bonus.js
@@ -0,0 +1,53 @@
+// (function(){
+
+"use strict";
+
+// function makeCar(vehicleMake, vehicleModel) {
+//     return {make: vehicleMake, model: vehicleModel};
+// }
+//
+// console.log(makeCar("VW", "beetle"));
+
+//-------------------------------------------------------------//
+
+// var cars = {
+//     f150: {
+//        make: "Ford",
+//        model: "F-150",
+//     } ,
+//     beetle: {
+//         make: "Volkswagen",
+//         model: "Beetle",
+//     } ,
+//     wrangler: {
+//         make: "Jeep",
+//         model: "Wrangler",
+//     }
+// }
+
+// var carController = {
+//     findMyCar: function(userCarChoice) {
+//         var userMakeChoice = userCarChoice.make;
+//         var userModelChoice = userCarChoice.model;
+//         carView.viewCar(userMakeChoice, userModelChoice);
+//     }
+// }
+//
+// var carView = {
+//     viewCar: function(userMake, userModel) {
+//         console.log("Car make: " + userMake);
+//         console.log("Car model: " + userModel);
+//     }
+// }
+//
+// carController.findMyCar(cars.f150);
+// carController.findMyCar(cars.beetle);
+// carController.findMyCar(cars.wrangler);
+
+//-------------------------------------------------------------//
+
+
+
+
+
+// });
