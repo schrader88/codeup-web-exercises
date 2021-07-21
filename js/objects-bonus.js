689d48f24ab655d18d4ba768f2e98b798c20dced
Kyle Schrader <kyle.schrader97@gmail.com>
2021-07-21 12:14:35 -0500

    finished third objects bonus question.

--- js/objects-bonus.js
+++ js/objects-bonus.js
@@ -46,7 +46,30 @@
 
 //-------------------------------------------------------------//
 
+// var input = [
+//         {
+//             name: "Chompers",
+//             breed: "Pug",
+//             age: 7
+//         },
+//         {
+//             name: "Freddy",
+//             breed: "Lab",
+//             age: 4
+//         },
+//         {
+//             name: "Mr. Pig",
+//             breed: "Mastif",
+//             age: 10
+//         }
+//     ];
+//
+// input.forEach(function getOlder(enteredArray) {
+//     ++enteredArray.age;
+//     console.log(enteredArray);
+// });
 
+//-------------------------------------------------------------//
 
 
 
