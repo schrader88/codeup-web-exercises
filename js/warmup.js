ce4333da0d04c622f43dfc56bfe8bdbc1025e401
Kyle Schrader <kyle.schrader97@gmail.com>
2021-07-27 09:13:34 -0500

    first save to javascript warmup on 7/27. created and linked files.

---
+++ js/warmup.js
@@ -0,0 +1,28 @@
+"use strict";
+
+// ================================= WARM UP
+// In a new HTML file in codeup-web-exercises, add a script to solve the following problem...
+// Write a function, getSimpleUser, that takes in a user object and returns a simplified user object.
+//     Assume the following shape of the user object for the function input:
+//     {
+//         firstName: '...',
+//         lastName: '...',
+//         email: '...',
+//         username: '...',
+//         password: '...'
+//     }
+// The shape of the output should be the following:
+// {
+//     firstName: '...',
+//         lastName: '...',
+// }
+// Regardless of the case of the first name and last name input, the output should be in all lower case.
+// var fred = {
+//     firstName: 'Fred',
+//     lastName: 'Smith',
+//     email: 'fred@email.com',
+//     username: 'fred123',
+//     password: 'pass123'
+// }
+// getSimpleUser(fred) // {firstName: 'fred', lastName: 'smith'}
+
