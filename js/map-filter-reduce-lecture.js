94189b847dfcf903042188aa621422a11561e9f9
Kyle Schrader <kyle.schrader97@gmail.com>
2021-08-26 12:00:56 -0500

    making progress through .reduce.

--- js/map-filter-reduce-lecture.js
+++ js/map-filter-reduce-lecture.js
@@ -61,33 +61,33 @@
 // $("#output").html(output);
 
 
-const cars = [
-    {
-        make: "Honda",
-        model: "Civic",
-        mileage: 10428
-    },
-    {
-        make: "Toyota",
-        model: "Corolla",
-        mileage: 9425
-    },
-    {
-        make: "Ford",
-        model: "Mustang",
-        mileage: 2567
-    },
-    {
-        make: "Audi",
-        model: "A3",
-        mileage: 14500
-    },
-    {
-        make: "Mazda",
-        model: "3",
-        mileage: 11248
-    }
-];
+// const cars = [
+//     {
+//         make: "Honda",
+//         model: "Civic",
+//         mileage: 10428
+//     },
+//     {
+//         make: "Toyota",
+//         model: "Corolla",
+//         mileage: 9425
+//     },
+//     {
+//         make: "Ford",
+//         model: "Mustang",
+//         mileage: 2567
+//     },
+//     {
+//         make: "Audi",
+//         model: "A3",
+//         mileage: 14500
+//     },
+//     {
+//         make: "Mazda",
+//         model: "3",
+//         mileage: 11248
+//     }
+// ];
 
 // const mileages = cars.map((car) => {return car.mileage});
 // const mileages = cars.map((car) => car.mileage);
@@ -98,7 +98,7 @@ const cars = [
 
 // .filter()
 
-const under10k = cars.filter(car => car.mileage < 10000);
+// const under10k = cars.filter(car => car.mileage < 10000);
 
 // Below is long-hand:
 
@@ -124,3 +124,47 @@ const under10k = cars.filter(car => car.mileage < 10000);
 // });
 
 // .reduce()
+
+// let totalCost = affordablesWithTax.reduce((total, indivItemCost) => total + indivItemCost)
+
+// var totalCost = affordablesWithTax.reduce(function(total, itemPrice){
+//    return total + itemPrice;
+// });
+
+// let totalCost = affordablesWithTax.reduce((total, indivItemCost, index) => {
+//    console.log(`The index is ${index}, the total is ${total}, the itemCost is ${indivItemCost}`);
+//    return total + indivItemCost;
+// });
+
+// let averagePrice = affordablesWithTax.reduce((total, indivItemCost, index, array) => {
+//    total = total + indivItemCost;
+//    if (index === array.length - 1) {
+//        return total/array.length;
+//    } else {
+//        return total;
+//    }
+// });
+
+// let prices = [32.99, 21.99, 6.99, 4.99, 12.99, 8.98, 5.99];
+// var total = prices.reduce(function (total, price, index){
+//     console.log(`The index is ${index}, the total is ${total}, the itemCost is ${price}`);
+//     return total + price;
+// }, 158.76);
+
+const aesopAnimals = ["Ant", "Grasshopper", "Ape", "Fox", "Donkey", "Donkey", "Donkey", "Lion", "Fly", "Bear", "Beaver", "Blackbird", "Wolf", "Bull", "Lion", "Cat", "Mouse", "Crab", "Fox", "Rooster", "Rooster", "Dog", "Fox", "Crow", "Crow", "Sheep", "Crow", "Snake", "Deer", "Dog", "Dog", "Sheep", "Dog", "Wolf", "Dog", "Lion", "Dove", "Ant", "Eagle", "Fox", "Eagle", "Stork", "Viper", "Fish", "Fly", "Ant", "Fly", "Snake", "Fox", "Crow", "Fox", "Fox", "Lion", "Fox", "Fox", "Lion", "Fox", "Stork", "Fox", "Weasel", "Fox", "Fly", "Hedgehog", "Hare", "Frog", "Fox", "Frog", "Mouse", "Frog", "Ox", "Frog", "Frog", "Goat", "Goose", "Hare", "Horse", "Donkey", "Horse", "Kite", "Dove", "Lion", "Lion", "Lion", "Lion", "Bear", "Fox", "Lion", "Boar", "Vulture", "Dog", "Mouse", "Oyster", "Donkey", "Ox", "Kite", "Snake", "Crab", "Snake", "Snake", "Tortoise", "Tortoise", "Hare", "Cat", "Wolf", "Wolf", "Lamb", "Swallow", "Tortoise"];
+
+// When total[animal] = 1, I create a key value pair: Ex: ant: 1.
+
+// let aesopsAnimalsCounted = aesopAnimals.reduce((total, animal) => {
+//     total[animal] = 1;
+//     return total;
+// }, {});
+
+let aesopsAnimalsCounted = aesopAnimals.reduce((total, animal) => {
+    if (!total[animal]) {
+        total[animal] = 1;
+    } else {
+        total[animal] += 1;
+    }
+    return total;
+}, {});
