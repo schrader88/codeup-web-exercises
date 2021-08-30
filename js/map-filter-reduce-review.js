f0f899e337e26273b6312c2e90baccb7a976681c
Kyle Schrader <kyle.schrader97@gmail.com>
2021-08-30 08:45:31 -0500

    finished Array Squared example.

--- js/map-filter-reduce-review.js
+++ js/map-filter-reduce-review.js
@@ -1,2 +1,16 @@
 "use strict";
 
+
+// Array Squared Problem
+
+const input = [1, 2, 3, 4, 5];
+// We want [1, 4, 9, 16, 25];
+
+const output = input.map(number => number**2)
+
+// console.log(output);
+
+// Sum of all POSITIVE element(s)
+
+const input2 = [1, -4, 12, 0, -3, ]
+
