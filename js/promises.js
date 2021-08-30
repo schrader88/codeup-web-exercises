0db2010ebb584d385bddd0468139516a3d096aa1
Kyle Schrader <kyle.schrader97@gmail.com>
2021-08-30 14:20:45 -0500

    done with MVP of promise exercise. refactoring.

--- js/promises.js
+++ js/promises.js
@@ -1,2 +1,12 @@
 "use strict";
 
+// fetch(`https://api.github.com/users`, {headers: {'Authorization': GITHUB_API_TOKEN}});
+
+function lastPushEvent(username) {
+    return fetch(`https://api.github.com/users/${username}/events`, {headers: {'Authorization': 'token' + GITHUB_API_TOKEN}})
+        .then(response => response.json())
+        .then(response => response[0].created_at)
+        .catch(console.error)
+}
+
+console.log(lastPushEvent("schrader88"));
