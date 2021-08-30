c08036c3da53faa984606e6b349e9c0c457a5993
Kyle Schrader <kyle.schrader97@gmail.com>
2021-08-30 08:54:40 -0500

    finished sum of positive elements problem

--- js/map-filter-reduce-review.js
+++ js/map-filter-reduce-review.js
@@ -6,11 +6,16 @@
 const input = [1, 2, 3, 4, 5];
 // We want [1, 4, 9, 16, 25];
 
-const output = input.map(number => number**2)
+const output = input.map(number => number**2);
 
 // console.log(output);
 
 // Sum of all POSITIVE element(s)
 
-const input2 = [1, -4, 12, 0, -3, ]
+const input2 = [1, -4, 12, 0, -3, 29, -150];
+// We want 42
+
+const output2 = input2.filter(number => number >= 0).reduce((total, number) => total + number);
+
+// console.log(output2);
 
