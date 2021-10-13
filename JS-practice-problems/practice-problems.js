d46c422a8cae75d52d6c33441a67fc8c78b0a813
Kyle Schrader <kyle.schrader97@gmail.com>
2021-10-13 12:35:36 -0500

    finished setting up files for practice JS problems. making first commit then completing examples

---
+++ JS-practice-problems/practice-problems.js
@@ -0,0 +1,17 @@
+// Create a function that takes two numbers as arguments and return their sum.
+
+// addition(3, 2) ➞ 5
+// addition(-3, -6) ➞ -9
+// addition(7, 3) ➞ 10
+
+// Write a function that takes an integer minutes and converts it to seconds.
+
+// convert(5) ➞ 300
+// convert(3) ➞ 180
+// convert(2) ➞ 120
+
+// Create a function that takes a number as an argument, increments the number by +1 and returns the result.
+
+// addition(0) ➞ 1
+// addition(9) ➞ 10
+// addition(-3) ➞ -2
