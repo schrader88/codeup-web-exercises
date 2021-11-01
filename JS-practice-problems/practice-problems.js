72fbbb462b9944af8a0eb1dff03f1707e731d4bd
Kyle Schrader <kyle.schrader97@gmail.com>
2021-11-01 17:49:47 -0500

    completed JS warmup for the day

--- JS-practice-problems/practice-problems.js
+++ JS-practice-problems/practice-problems.js
@@ -317,3 +317,32 @@ console.log(arrayOfMultiples(17, 6));
 // arrayOfMultiples(7, 5) ➞ [7, 14, 21, 28, 35]
 // arrayOfMultiples(12, 10) ➞ [12, 24, 36, 48, 60, 72, 84, 96, 108, 120]
 // arrayOfMultiples(17, 6) ➞ [17, 34, 51, 68, 85, 102]
+
+// Create a function that takes a country's name and its area as arguments and returns the area of the country's proportion of the total world's landmass.
+
+function areaOfCountry(name, area) {
+    let perc = (area/148940000) * 100;
+    return name + " is " + perc.toFixed(2) + "% of the world's landmass."
+}
+
+console.log(areaOfCountry("Russia", 17098242));
+console.log(areaOfCountry("USA", 9372610));
+console.log(areaOfCountry("Iran", 1648195));
+
+// areaOfCountry("Russia", 17098242) ➞ "Russia is 11.48% of the total world's landmass"
+// areaOfCountry("USA", 9372610), "USA is 6.29% of the total world's landmass"
+// areaOfCountry("Iran", 1648195) ➞ "Iran is 1.11% of the total world's landmass"
+
+// Create a function that squares every digit of a number.
+// IN PROGRESS //
+
+function squareDigits(number) {
+    let numToString = number.toString();
+    let numToArray = numToString.split().map(function (value) {
+        return parseInt(value);
+    });
+}
+
+// squareDigits(9119) ➞ 811181
+// squareDigits(2483) ➞ 416649
+// squareDigits(3212) ➞ 9414
