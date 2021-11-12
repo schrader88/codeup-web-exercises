d636327774cc8db34edeef93579fc41bc4a0e671
Kyle Schrader <kyle.schrader97@gmail.com>
2021-11-12 11:30:59 -0600

    finished JS warmup for the day

--- JS-practice-problems/practice-problems.js
+++ JS-practice-problems/practice-problems.js
@@ -355,3 +355,22 @@ console.log(calculator(4, "/", 2));
 // calculator(2, "*", 2) ➞ 4
 // calculator(4, "/", 2) ➞ 2
 
+// Take an array of integers (positive or negative or both) and return the sum of the absolute value of each element.
+
+function getAbsSum(numArr) {
+    let accumulator = 0;
+    numArr.forEach(function (num){
+        accumulator += Math.abs(num);
+    });
+    return accumulator;
+}
+
+console.log(getAbsSum([2, -1, 4, 8, 10]));
+console.log(getAbsSum([-3, -4, -10, -2, -3]));
+console.log(getAbsSum([2, 4, 6, 8, 10]));
+console.log(getAbsSum([-1]));
+
+// getAbsSum([2, -1, 4, 8, 10]) ➞ 25
+// getAbsSum([-3, -4, -10, -2, -3]) ➞ 22
+// getAbsSum([2, 4, 6, 8, 10]) ➞ 30
+// getAbsSum([-1]) ➞ 1
