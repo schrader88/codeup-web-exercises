eb2d72e8634f72b7e3670e8788acae4abf90659f
Kyle Schrader <kyle.schrader97@gmail.com>
2021-11-15 15:53:58 -0600

    finished JS warmup for the day

--- JS-practice-problems/practice-problems.js
+++ JS-practice-problems/practice-problems.js
@@ -374,3 +374,22 @@ console.log(getAbsSum([-1]));
 // getAbsSum([-3, -4, -10, -2, -3]) ➞ 22
 // getAbsSum([2, 4, 6, 8, 10]) ➞ 30
 // getAbsSum([-1]) ➞ 1
+
+// Create a function that takes an array of numbers and return "Boom!" if the digit 7 appears in the array. Otherwise, return "there is no 7 in the array".
+
+function sevenBoom(userArr) {
+    let stringOfArr = userArr.join("");
+    if (stringOfArr.indexOf("7") > -1) {
+        console.log("Boom!")
+    } else {
+        console.log("there is no 7 in the array")
+    }
+}
+
+// sevenBoom([1, 2, 3, 4, 5, 6, 7]) ➞ "Boom!"
+// sevenBoom([8, 6, 33, 100]) ➞ "there is no 7 in the array"
+// sevenBoom([2, 55, 60, 97, 86]) ➞ "Boom!"
+
+sevenBoom([1, 2, 3, 4, 5, 6, 7]);
+sevenBoom([8, 6, 33, 100]);
+sevenBoom([2, 55, 60, 97, 86]);
